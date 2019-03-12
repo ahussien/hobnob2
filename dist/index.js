0334277741e2af42ac046854ceaa95a4a95cb7f5
Ahmed Hussein Jomaa Soliman <ahsoliman@DSC.local>
2019-03-12 13:43:24 +0400

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var _http = _interopRequireDefault(require("http"));
+
+require("@babel/polyfill");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const requestHandler = function (req, res) {
+  res.writeHead(200, {
+    'Content-Type': "text/plain"
+  });
+  res.end('Hello, World!');
+};
+
+const server = _http.default.createServer(requestHandler);
+
+server.listen(8008); // Hello
+// oknpm i
