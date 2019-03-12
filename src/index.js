3522d9e8421a08ed56ac29c3d0c88e630553bd1b
Ahmed Hussein Jomaa Soliman <ahsoliman@DSC.local>
2019-03-12 13:44:17 +0400

    Fixing the index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import http from 'http';
 import '@babel/polyfill';
 
 const requestHandler = function (req, res) {
-  res.writeHead(200, { 'Content-Type': "text/plain" });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, World!');
 };
 const server = http.createServer(requestHandler);
