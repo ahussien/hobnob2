261c5d705c74165ea388cad6fdc29269d0209d80
Ahmed Hussein <ahussien.gomaa@gmail.com>
2019-04-14 14:11:51 +0400

    validating name

--- src/handlers/users/create.js
+++ src/handlers/users/create.js
@@ -1,6 +1,24 @@
 import createUserEngine from '../../engines/users/createUser'
+import Ajv from 'ajv';
+import profileSchema from '../../schema/users/profile.json';
+import createUserSchema from '../../schema/users/create.json';
+
 function create(req, res, db) {
-    createUserEngine(req,db).then((result) => {
+  //validate User input
+
+  const ajvValidate = new Ajv({allErrors: true})
+    .addFormat('email', /^[\w.+]+@\w+\.\w+$/)
+    .addSchema([profileSchema, createUserSchema])
+    .compile(createUserSchema);
+
+  const valid = ajvValidate(req.body);
+  if (!valid) {
+    res.status(400);
+    res.set('Content-Type', 'application/json');
+    res.json(ajvValidate.errors);
+  }
+
+  createUserEngine(req,db).then((result) => {
       res.status(201);
       res.set('Content-Type', 'text/plain');
       res.send(result._id);
